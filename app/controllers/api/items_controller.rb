c1118c6aa9aad8951b73b3484b1b6838c1f31af8
Mark <markandrewhamm@gmail.com>
2020-07-14 10:27:25 -0500

    RESTful routes

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,38 @@
 class Api::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @item = Item.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
+
+  def create
+    @item = Item.new(
+      name: params[:name],
+      description: params[:description],
+      price: params[:price]
+    )
+    @item.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @item = Item.find_by(id: params[:id])
+    @item.update(
+      name: params[:name] || @item.name,
+      description: params[:description] || @item.description,
+      price: params[:price] || @item.price,
+    )
+    @item.save
+    render 'show.json.jb'
+  end
+
+  def destroy
+    @item = Item.find_by(id: params[:id])
+    @item.destroy
+    render json: {message: 'The item has been deleted.'}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/items" => "items#index"
+    get "/items/:id" => "items#show"
+    post "/items" => "items#create"
+    patch "/items/:id" => "items#update"
+    delete "/items/:id" => "items#destroy"
+  end
 end
